9824fe9132113036d5ffef5baad2dc099236bdcd
S1rCoDy <148563516+S1rCoDy@users.noreply.github.com>
2023-12-14 16:46:40 +0500

    second files

---
+++ src/components/second.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './second.css';
+
+const Second = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <a href="">
+                    <img src="" alt="" />
+                </a>
+
+                <nav>
+                    <ul className="li">
+                        <li><a href="#">Туры</a></li>
+                        <li><a href="#">Круизы</a></li>
+                        <li><a href="#">О компании</a></li>
+                        <li><a href="#">Контакты</a></li>
+                        <li><a href="#">Виза</a></li>
+                    </ul>
+                    <p className="phone"><a href="https://pornhub.com" target="_blank">+7 800 555 3535</a></p>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Second;
